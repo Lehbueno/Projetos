03d65a5465c404188753fe0a3427dfbd0f854291
Lehbueno <buenoleticia07@gmail.com>
2024-08-17 11:11:31 -0300

    buscacorreiosok

--- buscacorreios.py
+++ buscacorreios.py
@@ -8,7 +8,18 @@ from selenium.webdriver.common.by import By
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 
-navegador.get(https://www2.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm)
+navegador.get('https://www2.correios.com.br/sistemas/buscacep/buscaCepEndereco.cfm')
 
-navegador.find_element('xpath', //a[contains(.,'Endereço por CEP')]).click()
-navegador.find_element(By.ID, 'cep').send_keys(69005-040)
+navegador.find_element('xpath', "//a[contains(.,'Endereço por CEP')]").click()
+navegador.find_element(By.ID, 'cep').click()
+navegador.find_element(By.ID, 'cep').send_keys('69005-040')
+navegador.find_element('xpath', "//input[contains(@type,'Submit')]").click()
+
+sleep(5)
+
+navegador.find_element('xpath', "//a[contains(.,'CEP ou Endereço')]").click()
+navegador.find_element('xpath', "//input[contains(@align,'left')]").click()
+navegador.find_element('xpath', "//input[contains(@align,'left')]").send_keys('Manaus')
+navegador.find_element('xpath', "//input[contains(@type,'Submit')]").click()
+
+navegador.quit
--- trivago.py
+++ trivago.py
@@ -8,13 +8,15 @@ from selenium.webdriver.common.by import By
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
 
-navegador.get(https://www.trivago.com.br/)
+navegador.get('https://www.trivago.com.br/')
 
-navegador.find_element('xpath', '//input[contains(@autocapitalize,'off')]').send_keys('Manaus')
-navegador.find_element('xpath', '//button[@type='button'][contains(.,'Pesquisar')]').click()
+sleep(5)
+
+navegador.find_element('xpath', "//input[contains(@autocapitalize,'off')]").send_keys('Manaus')
+navegador.find_element('xpath', "//button[@type='button'][contains(.,'Pesquisar')]").click()
 
 find_sorting_selector = \
-navegador.find_element('xpath', '//select[contains(@class,'SortingSelector_select__usK9k Select_select__7NVo3 truncate_truncate__vCzPM')]').click()
+navegador.find_element('xpath', "//select[contains(@class,'SortingSelector_select__usK9k Select_select__7NVo3 truncate_truncate__vCzPM')]").click()
 select_sorting_selector = Select(find_sorting_selector)
 select_sorting_selector.select_by_visible_text('Avaliação e sugestões')
 
