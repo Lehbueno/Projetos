d3848c3a1596e482ab94c40121bce76991ffdc86
Lehbueno <buenoleticia07@gmail.com>
2024-08-17 10:41:33 -0300

    correios

--- trivago.py
+++ trivago.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 
 servico = Service(ChromeDriverManager().install())
 navegador = webdriver.Chrome(service=servico)
@@ -18,3 +19,7 @@ select_sorting_selector = Select(find_sorting_selector)
 select_sorting_selector.select_by_visible_text('Avaliação e sugestões')
 
 sleep(5)
+
+first_result = navegador.find_element(By.CSS_SELECTOR, 'li[data-testid="accommodation-list-element"]:first-child h2 span')
+for e in first_result:
+    print(e.text)
